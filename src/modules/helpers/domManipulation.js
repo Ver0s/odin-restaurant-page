3182f1d1ad336241406702317e7c855e5ec1e4da
Arek <arek.stachyra41@gmail.com>
2022-08-17 13:35:33 +0200

    add dom manipulation helper functions

---
+++ src/modules/helpers/domManipulation.js
@@ -0,0 +1,18 @@
+// https://dev.to/ahmedadel/custom-javascript-createelement-function-244f
+function createElement(elObj) {
+    const element = document.createElement(elObj.tag);
+    for (const prop in elObj) {
+        element[prop] = elObj[prop];
+    }
+    return element;
+}
+
+function appendChildren(parent, ...children) {
+    for (const child of children){
+        parent.appendChild(child);
+    }
+
+    return parent;
+}
+
+export { createElement, appendChildren}
